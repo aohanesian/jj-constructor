e6e0b87989b5f59166aca919e79d39b2111d386a
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-11-07 19:23:06 +0200

    comm

--- main.js
+++ main.js
@@ -8,46 +8,52 @@ function Student(name, surname, yearOfBirth) {
     this.attendance = Array.from({length: 10}).fill(null);
 }
 
-Student.prototype.getAge = function () {
-    return new Date().getFullYear() - this.yearOfBirth;
+const func = {
+    getAge() {
+        return new Date().getFullYear() - this.yearOfBirth;
+    },
+    summary() {
+        const outputGrade = this.grade.filter(item => item !== null).reduce((a, b) => (a + b)) / this.grade.filter(item => item !== null).length;
+        const outputAttendance = this.attendance.filter(item => item !== null).reduce((a, b) => (a + b)) / this.grade.filter(item => item !== null).length;
+        switch (true) {
+            case outputGrade > 9 && outputAttendance > 0.9:
+                return `Ути какой молодчинка!`;
+            case outputGrade < 9 && outputAttendance < 0.9:
+                return `Редиска!`;
+            case outputGrade < 9 || outputAttendance < 0.9:
+                return `Норм, но можно лучше!`;
+        }
+    },
+    present() {
+        if (this.attendance.length > 10) throw `must be below 10`;
+        let emptyIndex = this.attendance.indexOf(null);
+        if (emptyIndex === -1) throw `Student ${this.name} ${this.surname} visited all classes, cant check attendance`
+        this.attendance[emptyIndex] = true;
+    },
+    absent() {
+        if (this.attendance.length > 10) throw `must be below 10`;
+        let emptyIndex = this.attendance.indexOf(null)
+        if (emptyIndex === -1) throw `No more space in ${this.attendance}`
+        this.attendance[emptyIndex] = false;
+    },
+    mark(number) {
+        if (this.grade.length > 10) throw `must be below 10`;
+        if (typeof number !== `number`) throw `enter a number`
+        if (number > 10 || number < 0) throw `enter from 0 to 10`
+        let emptyIndex = this.grade.indexOf(null)
+        if (emptyIndex === -1) throw `Student ${this.name} ${this.surname} visited all classes, cant give more marks`
+        this.grade[emptyIndex] = number;
+    },
 }
-Student.prototype.summary = function () {
-    const outputGrade = this.grade.filter(item => item !== null).reduce((a, b) => (a + b)) / this.grade.filter(item => item !== null).length;
-    const outputAttendance = this.attendance.filter(item => item !== null).reduce((a, b) => (a + b)) / this.grade.filter(item => item !== null).length;
-    switch (true) {
-        case outputGrade > 9 && outputAttendance > 0.9:
-            return `Ути какой молодчинка!`;
-        case outputGrade < 9 && outputAttendance < 0.9:
-            return `Редиска!`;
-        case outputGrade < 9 || outputAttendance < 0.9:
-            return `Норм, но можно лучше!`;
-    }
+
+for (const funcKey in func) {
+    Student.prototype[funcKey] = func[funcKey]
 }
-Student.prototype.present = function () {
-    if (this.attendance.length > 10) throw `must be below 10`;
-    let emptyIndex = this.attendance.indexOf(null);
-    if (emptyIndex === -1) throw `Student ${this.name} ${this.surname} visited all classes, cant check attendance`
-    this.attendance[emptyIndex] = true;
-}
-Student.prototype.absent = function () {
-    if (this.attendance.length > 10) throw `must be below 10`;
-    let emptyIndex = this.attendance.indexOf(null)
-    if (emptyIndex === -1) throw `No more space in ${this.attendance}`
-    this.attendance[emptyIndex] = false;
-}
-Student.prototype.mark = function (number) {
-    if (this.grade.length > 10) throw `must be below 10`;
-    if (typeof number !== `number`) throw `enter a number`
-    if (number > 10 || number < 0) throw `enter from 0 to 10`
-    let emptyIndex = this.grade.indexOf(null)
-    if (emptyIndex === -1) throw `Student ${this.name} ${this.surname} visited all classes, cant give more marks`
-    this.grade[emptyIndex] = number;
-}
+
 let student1 = new Student(`Artur`, `Ohanesian`, 1994);
 let student2 = new Student(`Oleksandr`, `Solianyk`, 1998);
 let student3 = new Student(`Darya`, `Shevchenko`, 2002);
 let student4 = new Student(`Kristina`, `Tymofiichuk`, 2002);
-
 
 // student1
 student1.absent();
